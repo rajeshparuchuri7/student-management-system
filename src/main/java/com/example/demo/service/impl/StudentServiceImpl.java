6233c5337c4e8f4e83670ca828677131dee6deb2
rajeshparchuri7 <rajeshparchuri7502@gmail.com>
2026-07-16 16:35:23 +0530

    Initial commit - Student Management System

---
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -0,0 +1,68 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repositary.StudentRepository;
+import com.example.demo.service.StudentService;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+	
+	private StudentRepository studentRepository;
+	
+	
+
+	public StudentServiceImpl(StudentRepository studentRepository) {
+		super();
+		this.studentRepository = studentRepository;
+	}
+
+
+
+	@Override
+	public List<Student> getAllStudents() {
+		
+		return studentRepository.findAll();
+	}
+	
+	@Override
+	public Student saveStudent(Student student) {
+		return studentRepository.save(student);
+	}
+
+
+
+	@Override
+	public Student getStudentById(Long id) {
+		
+		return studentRepository.findById(id).get();
+	}
+
+
+
+	@Override
+	public Student updateStudent(Student student) {
+		
+		return studentRepository.save(student);
+	}
+
+
+
+	@Override
+	public void deleStudentById(Long id) {
+		
+		studentRepository.deleteById(id);
+		
+	}
+
+
+     @Override
+	public List<Student> searchStudents(String keyword) {
+	    return studentRepository.findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(keyword,keyword);
+	    
+	}
+
+}
